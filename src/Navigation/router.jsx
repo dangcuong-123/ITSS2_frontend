1c036b814722ee499d7a397d8bdeefffb51a8ea9
MaiPhong <phong.mt184172@gmail.com>
2022-12-07 15:22:53 +0700

    Resolve conflict

--- src/Navigation/router.jsx
+++ src/Navigation/router.jsx
@@ -5,6 +5,10 @@ import RegisterScreen from "../Screen/User/Login/RegisterScreen";
 import PasswordForgotScreen from "../Screen/User/Login/PasswordForgotScreen";
 import HomePageScreen from "../Screen/HomePage/HomePageScreen";
 import Sidebar from "../components/Sidebar/Sidebar";
+import AddRestaurant from "../Screen/Restaurant/Add1"
+import EditRestaurant from "../Screen/Restaurant/Edit"
+import AddHotel from "../Screen/Hotel/Add"
+import EditHotel from "../Screen/Hotel/Edit";
 import SearchPlan from "../components/SearchPlan/SearchPlan";
 import SearchPlanRestaurant from "../components/SearchPlan/SearchPlanRestaurant";
 const HomeSidebar = () => {
@@ -29,6 +33,10 @@ const Router = () => {
         <Route path="login" element={<LoginScreen />} />
         <Route path="register" element={<RegisterScreen />} />
         <Route path="password_forgot" element={<PasswordForgotScreen />} />
+        <Route path="addRes" element={<AddRestaurant></AddRestaurant>}/>
+        <Route path="editRes" element={<EditRestaurant></EditRestaurant>}/>
+        <Route path="addHotel" element={<AddHotel></AddHotel>}></Route>
+        <Route path="editHotel" element={<EditHotel></EditHotel>}></Route>
         <Route path="/*" element={<HomeSidebar />} />
       </Routes>
     </BrowserRouter>
