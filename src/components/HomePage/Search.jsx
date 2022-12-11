db25c6ba3153fdcac354b4d6c26154a8ac2f911e
phong.hoang <phong.hoang@linkbal.co.jp>
2022-12-11 21:33:16 +0700

    fix search bar

--- src/components/HomePage/Search.jsx
+++ src/components/HomePage/Search.jsx
@@ -1,29 +1,17 @@
 import React from "react";
 import "../../style/search.css";
-
+import Input from "../Input";
+import ButtonSearch from "../Button";
+import { AdminTitle } from "../../style";
 const Search = ({ text, search, onSearchChange }) => (
   <div>
-    <div className="searchBox p2">
-      <div className="text-2xl font-bold items-center"> {text} </div>
-      <input
-        id="userInput"
-        className="search tc bg-lightest-purple placeholder-white items-center"
-        type="search"
-        placeholder="What do you want to go..."
-        style={{
-          display: "block",
-          margin: "auto",
-          border: "1px solid white",
-          borderRadius: "30px",
-          height: "45px",
-          width: "500px",
-          outline: "none",
-          backgroundColor: "#30A1C9",
-        }}
-        value={search}
-        onChange={(text) => onSearchChange(text)}
-      />
-    </div>
+    <span className="text-2xl font-bold mb-5 mt-0">
+      Search plan
+    </span>
+    <div className="flex justify-between ml-2 mr-7">
+      <Input  placeholder="Where do you want to go ?" onChange={onSearchChange} />
+      <ButtonSearch color="font-bold mr-0">Search</ButtonSearch>
+    </div> 
   </div>
 );
 
