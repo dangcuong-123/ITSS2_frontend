6ec68fe744eec77790f024373486b3963fba0bf7
longvm2280 <longvm2280@gmail.com>
2022-12-18 01:19:31 +0700

    integrate api change price

--- src/services/HotelServices.js
+++ src/services/HotelServices.js
@@ -3,3 +3,7 @@ import { apiService } from "../common/helpers";
 export const getHotel = async () => {
   return await apiService.get("/hotel/show");
 };
+
+export const getHotelLowerEqualPrice = async (price) => {
+  return await apiService.get(`/hotel/search_hotel_lower_equal_price?price=${price}`);
+};
