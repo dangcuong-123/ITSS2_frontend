87cd7fc9547db9131bc23c6af8002c0737d40a32
longvm2280 <longvm2280@gmail.com>
2022-12-17 23:57:31 +0700

    integrate api search location

--- src/components/HomePage/Search.jsx
+++ src/components/HomePage/Search.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../style/search.css";
 import Input from "../Input";
 import ButtonSearch from "../Button";
 import { AdminTitle } from "../../style";
-const Search = ({ text, search, onSearchChange }) => (
-  <div>
-    <span className="text-2xl font-bold mb-5 mt-0">
-      Search plan
-    </span>
-    <div className="flex justify-between ml-2 mr-7">
-      <Input  placeholder="Where do you want to go ?" onChange={onSearchChange} />
-      <ButtonSearch color="font-bold mr-0">Search</ButtonSearch>
-    </div> 
-  </div>
-);
+function Search({onSearchChange}) {
+  const [value, setValue] = useState("");
+
+  const handleSearch = () => {
+    if (value != "") {
+      onSearchChange(value)
+    }
+  }
+
+  return (
+    <div>
+      <span className="text-2xl font-bold mb-5 mt-0">
+        Search plan
+      </span>
+      <div className="flex justify-between ml-2 mr-7">
+        <Input placeholder="Where do you want to go ?" value={value} onChange={e => setValue(e.target.value)} />
+        <ButtonSearch color="font-bold mr-0" onClick={handleSearch}>Search</ButtonSearch>
+      </div>
+    </div>
+  )
+}
 
 export default Search;
--- src/services/HomeServices.js
+++ src/services/HomeServices.js
@@ -3,3 +3,7 @@ import { apiService } from "../common/helpers";
 export const showHomePage = async () => {
   return await apiService.get("/homepage/show");
 };
+
+export const getHotelAndRestaurantList = async (location_name) => {
+  return await apiService.get(`/homepage/search_location_name?name=${location_name}`);
+};
