dafec580040003a7737a63dbf7c1defaf9d2c506
MaiPhong <phong.mt184172@gmail.com>
2022-12-18 21:56:09 +0700

    Delete hotel

--- src/services/HotelServices.js
+++ src/services/HotelServices.js
@@ -7,6 +7,7 @@ export const getHotel = async () => {
 export const getHotelById = async (id) => {
   return await apiService.get(`/hotel/search_hotel_id/${id}`);
 };
+
 export const getHotelLowerEqualPrice = async (price) => {
   return await apiService.get(`/hotel/search_hotel_lower_equal_price?price=${price}`);
 };
