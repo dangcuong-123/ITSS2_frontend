3a685c549cd39c80f64e978c95474ada9327cb55
MaiPhong <phong.mt184172@gmail.com>
2022-12-18 17:30:26 +0700

    merge api add/edit hotel

--- src/Screen/Admin/Hotel/EditHotel.js
+++ src/Screen/Admin/Hotel/EditHotel.js
@@ -1,14 +1,51 @@
-import { useState, React } from "react";
+import { useState, React, useEffect } from "react";
 import LayoutAdmin from "../../../components/Sidebar/AdminContainer";
 import data1 from "./mock-data.json";
 import { AdminTitle } from "../../../style";
 import Input from "../../../components/Input";
 import Button from "../../../components/Button";
 import { Link } from "react-router-dom";
+import Select from 'react-select'
+import { useParams } from 'react-router-dom'
+import { getHotelById } from "../../../services/HotelServices";
 
 const EditHotel = () => {
+  const { id } = useParams();
+  // console.log(id)
+  const [hotelID, setHotelID] = useState([]);
   const [editContactId1, setEditContactId1] = useState(null);
   const [contacts1, setContacts1] = useState(data1);
+
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [intro, setIntro] = useState("");
+  const [roomInfo, setRoomInfo] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("")
+  const [image, setImage] = useState("")
+  const [province, setProvince] = useState("")
+
+  const options = [
+    { value: 'ha long', label: 'Ha long' },
+    { value: 'ha noi', label: 'Ha noi' },
+    { value: 'hai phong', label: 'Hai phong' }
+  ]
+  useEffect(() => {
+    getHotelById(id)
+      .then((res) => {
+        setHotelID(res.data);
+        setName(res.data[0].hotel_name)
+        setAddress(res.data[0].hotel_address)
+        setProvince(options[res.data[0].location_id - 1].label)
+        setImage(res.data[0].image_url)
+        setDescription(res.data[0].hotel_description)
+        setPrice(res.data[0].hotel_fee)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    
+  }, []);
 
   const [editHotel, setEditHotel] = useState({
     name: "",
@@ -84,6 +121,12 @@ const EditHotel = () => {
   const [editRoomInfo, setEditRoomInfo] = useState("");
   const [editPrice, setEditPrice] = useState("");
 
+
+  const handleTypeSelect = e => {
+    setProvince(e.value);
+  };
+
+
   const handleEditClick = (e) => {
     e.preventDefault();
     const editHot = {
@@ -103,101 +146,142 @@ const EditHotel = () => {
     //         alert("Edit hotel complete");
     // })
   };
+  const handleClick = (e) => {
+    e.preventDefault();
+    const editHot = {
+      "hotel_id": id,
+      "hotel_name": name,
+      "hotel_address_input": address,
+      "hotel_address_select": province,
+      "image_url": image,
+      "hotel_description": description,
+      "hotel_fee": price
+    };
+    console.log(editHot);
+    fetch("http://35.78.85.107:8080/hotel/edit_hotel", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(editHot)
+    }).then(() => {
+      console.log("Edit hotel complete");
+      alert("Edit hotel complete");
+    })
 
+  }
   return (
     <LayoutAdmin>
       <div>
-        <AdminTitle>Add/Edit Hotel</AdminTitle>
-        <div className="flex items-center">
-          <div className="w-1/5 self-center text-end">
-            <label className="text-black font-bold">
-              Name
-              <span className="text-red-700"> *</span>
-            </label>
-          </div>
-          <div className="w-3/5 items-center">
-            <Input
-              type="text"
-              placeholder="Name restaurant"
-              value={editName}
-              onChange={(e) => setEditName(e.target.value)}
-            />
-          </div>
-        </div>
+        <AdminTitle>Edit Hotel</AdminTitle>
 
-        <div className="flex items-center">
-          <div className="w-1/5 self-center text-end">
-            <label className="text-black font-bold">
-              Address
-              <span className="text-red-700"> *</span>
-            </label>
-          </div>
-          <div className="w-3/5 items-center">
-            <Input
-              type="text"
-              placeholder="Name restaurant"
-              value={editAddress}
-              onChange={(e) => setEditAddress(e.target.value)}
-            />
-          </div>
-        </div>
+              <div className="flex items-center">
+                <div className="w-1/5 self-center text-end">
+                  <label className="text-black font-bold">
+                    Name
+                    <span className="text-red-700"> *</span>
+                  </label>
+                </div>
+                <div className="w-3/5 items-center">
+                  <Input
+                    type="text"
+                    placeholder="Name hotel"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="w-1/5 self-center text-end">
+                  <label className="text-black font-bold">
+                    Address
+                    <span className="text-red-700"> *</span>
+                  </label>
+                </div>
 
-        <div className="flex items-center">
-          <div className="w-1/5 self-center text-end">
-            <label className="text-black font-bold">
-              Introduce
-              <span className="text-red-700"> *</span>
-            </label>
-          </div>
-          <div className="w-3/5 items-center">
-            <Input
-              type="text"
-              placeholder="Name restaurant"
-              value={editIntro}
-              onChange={(e) => setEditIntro(e.target.value)}
-            />
-          </div>
-        </div>
+                <div className="w-3/5 items-center">
+                  <Input
+                    type="text"
+                    placeholder="Address's hotel"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
+                </div>
 
-        <div className="flex items-center">
-          <div className="w-1/5 self-center text-end">
-            <label className="text-black font-bold">
-              Room information
-              <span className="text-red-700"> *</span>
-            </label>
-          </div>
-          <div className="w-3/5 items-center">
-            <Input
-              type="text"
-              placeholder="Name restaurant"
-              value={editRoomInfo}
-              onChange={(e) => setEditRoomInfo(e.target.value)}
-            />
-          </div>
-        </div>
+              </div>
 
-        <div className="flex items-center">
-          <div className="w-1/5 self-center text-end">
-            <label className="text-black font-bold">
-              Price per day
-              <span className="text-red-700"> *</span>
-            </label>
-          </div>
-          <div className="w-3/5 items-center">
-            <Input
-              type="text"
-              placeholder="Name restaurant"
-              value={editPrice}
-              onChange={(e) => setEditPrice(e.target.value)}
-            />
-          </div>
-        </div>
+              <div className="flex items-center">
+                <div className="w-1/5 self-center text-end">
+                  <label className="text-black font-bold">
+                    Province
+                    <span className="text-red-700"> *</span>
+                  </label>
+                </div>
+                <div className="items-center" style={{ padding: '15px' }}>
+                  <Select options={options} placeholder={province} onChange={handleTypeSelect} />
+                </div>
+              </div >
+
+              <div className="flex items-center">
+                <div className="w-1/5 self-center text-end">
+                  <label className="text-black font-bold">
+                    Description
+                    <span className="text-red-700"> *</span>
+                  </label>
+                </div>
+                <div className="w-3/5 items-center">
+                  <Input
+                    type="text"
+                    placeholder="Write something about your hotel"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center">
+                <div className="w-1/5 self-center text-end">
+                  <label className="text-black font-bold">
+                    Image URL
+                    <span className="text-red-700"> *</span>
+                  </label>
+                </div>
+                <div className="w-3/5 items-center">
+                  <Input
+                    type="text"
+                    placeholder="Link to image"
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center">
+                <div className="w-1/5 self-center text-end">
+                  <label className="text-black font-bold">
+                    Price per day
+                    <span className="text-red-700"> *</span>
+                  </label>
+                </div>
+                <div className="w-3/5 items-center">
+                  <Input
+                    type="text"
+                    placeholder="Price per day"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                  />
+                </div>
+              </div>
+
+        
       </div>
-      <div className="w-4/5 flex ml-4" style={{ justifyContent: "end" }}>
+      <div className="w-4/5 flex ml-4 justify-end">
         <Button color="from-[#961919] to-[#f6646e] font-bold">
-          <Link to="/list-hotel">Cancel</Link>
+          <Link to="/list-hotel">
+            Cancel
+          </Link>
         </Button>
-        <Button color="font-bold mr-0" onClick={handleEditClick}>
+        <Button color="font-bold mr-0" onClick={handleClick}>
+          <Link to='/list-hotel'>
+          </Link>
           Edit Hotel
         </Button>
       </div>
--- src/services/HotelServices.js
+++ src/services/HotelServices.js
@@ -3,3 +3,7 @@ import { apiService } from "../common/helpers";
 export const getHotel = async () => {
   return await apiService.get("/hotel/show");
 };
+
+export const getHotelById = async (id) => {
+  return await apiService.get(`/hotel/search_hotel_id/${id}`);
+};
