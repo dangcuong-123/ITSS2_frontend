82b3217314b89d4b20df93f20dc38379c08ec460
dangcuong-123 <cuong.dang@pionero.io>
2022-12-20 13:02:13 +0700

    update

--- src/services/HotelServices.js
+++ src/services/HotelServices.js
@@ -7,6 +7,7 @@ export const getHotel = async () => {
 export const searchHotel = async (nameHotel) => {
   return await apiService.get(`/hotel/search_hotel_name?name=${nameHotel}`);
 };
+
 export const getHotelLowerEqualPrice = async (price) => {
   return await apiService.get(
     `/hotel/search_hotel_lower_equal_price?price=${price}`
