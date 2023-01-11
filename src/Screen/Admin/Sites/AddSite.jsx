048593f772f806a7c3013096c8fcdc76d2170d9f
phong.hoang <phong.hoang@linkbal.co.jp>
2023-01-11 16:28:31 +0700

    handle add and view site

--- src/Screen/Admin/Sites/AddSite.jsx
+++ src/Screen/Admin/Sites/AddSite.jsx
@@ -9,30 +9,101 @@ import { useTranslation } from 'react-i18next';
 import { Link, useAsyncError } from "react-router-dom";
 import { Snackbar, Alert } from "@mui/material"
 import Select from 'react-select'
+import { MultiSelect } from "react-multi-select-component";
 
 const AddSite = () => {
     const { t } = useTranslation()
     const [name, setName] = useState("")
     const [address, setAddress] = useState("")
     const [intro, setIntro] = useState("")
-    const [type, setType] = useState("")
-    const [transportation, setTransportation] = useState("")
+    const [type, setType] = useState([])
+    const [transportation, setTransportation] = useState([])
     const [image, setImage] = useState("")
     const [province, setProvince] = useState("")
-
-    const options = [
-        { value: 'ha noi', label: 'Ha noi' },
-        { value: 'ha long', label: 'Ha long' },
-        { value: 'hai phong', label: 'Hai phong' }
+    const [description, setDescription] = useState("")
+    const [open, setOpen] = useState(false)
+    const [severity, setSeverity] = useState('')
+    const provinceOptions = [
+        { value: 'Hà Nội', label: 'Hà Nội' },
+        { value: 'Quảng Ninh', label: 'Quảng Ninh' },
+        { value: 'Hải Phòng', label: 'Hải Phòng' },
+        { value: 'Lào Cai', label: 'Lào Cai' },
+        { value: 'Huế', label: 'Huế' },
+        { value: 'Đà Nẵng', label: 'Đà Nẵng' },
+        { value: 'Khánh Hòa', label: 'Khánh Hòa' },
+        { value: 'TP Hồ Chí Minh', label: 'TP Hồ Chí Minh' },
+        { value: 'Cần Thơ', label: 'Cần Thơ' }
     ]
-
+    const typeOptions = [
+        { value: 'Biển', label: 'Biển' },
+        { value: 'Núi', label: 'Núi' },
+        { value: 'Danh lam thắng cảnh', label: 'Danh lam thắng cảnh' },
+        { value: 'Di tích lịch sử', label: 'Di tích lịch sử' },
+        { value: 'Chùa', label: 'Chùa' },
+        { value: 'Bảo tàng', label: 'Bảo tàng' }
+    ]
+    const transportationOptions = [
+        { value: 'Xe máy', label: 'Xe máy' },
+        { value: 'Ô tô', label: 'Ô tô' },
+        { value: 'Tàu hỏa', label: 'Tàu hỏa' },
+        { value: 'Thuyền', label: 'Thuyền' }
+    ]
+    var bike = 0, car = 0, ship = 0, train = 0
+    if(transportation.some(some => some.value === "Xe máy")) {
+        bike = 1
+    }
+    if(transportation.some(some => some.value === "Ô tô")) {
+        car = 1
+    }
+    if(transportation.some(some => some.value === "Tàu hỏa")) {
+        ship = 1
+    }
+    if(transportation.some(some => some.value === "Thuyền")) {
+        train = 1
+    }
+    const provinces = []
     const handleTypeSelect = e => {
         setProvince(e.value);
     };
+    const types = type.map(function(item) {
+        return item['value'];
+      });
+      console.log(province.value)
+    // console.log(types)
+    const handleClick = (e) =>{
+        e.preventDefault();
+        const addSite = { 
+          "location_name": name, 
+          "location_address": address, 
+          "location_description": description,
+          "image_url": image,
+          "loc_province": province.value,
+          "tags": JSON.stringify(types),
+          "train": train,
+          "motorbike": bike,
+          "car": car,
+          "ship": ship
 
-    const handleClick = (e) => {
-        console.log("click")
-    }
+        //   "location_description": description,
+        //   "location_fee":price
+        };
+        console.log(addSite);
+        fetch("http://13.230.246.62:8080/location/create",{
+          method:"POST",
+                headers:{"Content-Type" : "application/json"},
+                body:JSON.stringify(addSite)
+            }).then(()=>{
+                console.log("Add location complete");
+                alert("Add location complete");
+                setOpen(true);
+                setSeverity('success')
+        }).catch((err) =>{
+          console.log(err)
+          setSeverity('error')
+          setOpen(true)
+        })
+    
+      }
     return (
         <LayoutAdmin>
             <div>
@@ -82,7 +153,7 @@ const AddSite = () => {
                         </label>
                     </div>
                     <div className="items-center" style={{ padding: '10px' }}>
-                        <Select options={options} onChange={handleTypeSelect} />
+                        <Select options={provinceOptions} onChange={setProvince} />
                     </div>
                 </div >
 
@@ -97,9 +168,9 @@ const AddSite = () => {
                     <div className="w-3/5 items-center">
                         <Input
                             type="text"
-                            placeholder={t('addSite.address')}
-                            value={intro}
-                            onChange={(e) => setIntro(e.target.value)}
+                            placeholder="Mô tả"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
                         />
                     </div>
                 </div>
@@ -111,8 +182,8 @@ const AddSite = () => {
                             <span className="text-red-700"> *</span>
                         </label>
                     </div>
-                    <div className="items-center" style={{ padding: '10px' }}>
-                        <Select options={options} onChange={handleTypeSelect} />
+                    <div className="items-center" style={{ padding: '10px' , width: '400px'}}>
+                        <MultiSelect options={typeOptions} value={type} onChange={setType} labelledBy="Select" />
                     </div>
                 </div >
 
@@ -123,8 +194,8 @@ const AddSite = () => {
                             <span className="text-red-700"> *</span>
                         </label>
                     </div>
-                    <div className="items-center" style={{ padding: '10px' }}>
-                        <Select options={options} onChange={handleTypeSelect} />
+                    <div className="items-center" style={{ padding: '10px', width: '400px' }}>
+                        <MultiSelect options={transportationOptions} value={transportation} onChange={setTransportation} labelledBy="Select" />
                     </div>
                 </div >
 
@@ -154,7 +225,7 @@ const AddSite = () => {
                     <Button color="font-bold mr-0" onClick={handleClick}>
                         <Link to='/list-hotel'>
                         </Link>
-                        {t('sites.title')}
+                        Thêm địa danh
                     </Button>
                 </div>
             </div>
