adc7f4ff280e0426b1883b29787bac9d35ee54fb
minhtan <32776120+minhtan4711@users.noreply.github.com>
2023-01-19 17:20:32 +0700

    feature: comment ui

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Input from "../../components/Input";
+import Button from "../Button";
+
+export const Comment = () => {
+	return (
+		<div>
+			<div className="flex justify-between">
+				<div className="flex items-center">
+					<img className="w-10 h-10 rounded-full" src="" alt="" />
+					<div className="ml-3">
+						<p className="text-sm font-medium text-gray-900">Comment name</p>
+						<div className="flex space-x-1 text-sm text-gray-500">
+							<time dateTime="2020-03-16">Comment date</time>
+						</div>
+					</div>
+				</div>
+			</div>
+			<p className="mt-3 text-sm text-gray-700">Comment</p>
+			<hr className="mb-8" />
+			<div className="pb-8">
+				<Input type="text" placeholder="Commnet" />
+				<Button>Send</Button>
+			</div>
+		</div>
+	);
+};
