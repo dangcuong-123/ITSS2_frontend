a6f22314665f7728951b4ff39cd0197658c3491b
caohieu-281 <kvlingxss@gmail.com>
2023-02-16 00:54:08 +0700

    list plan, delete plan

--- src/Screen/Admin/Plan/ListPlan.jsx
+++ src/Screen/Admin/Plan/ListPlan.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import LayoutAdmin from "../../../components/Sidebar/AdminContainer";
-import { showPlan } from "../../../services/PlanServices";
+import { getPlanByUserName } from "../../../services/PlanServices";
 import { AdminTitle } from "../../../style";
-import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
+import DeletePlan from "./DeletePlan";
+
 const ListPlan = () => {
   const [listPlan, setListPlan] = useState();
   useEffect(() => {
-    showPlan()
+    getPlanByUserName(sessionStorage.getItem("username"))
       .then((res) => {
         setListPlan(res.data.plans);
       })
@@ -15,6 +15,7 @@ const ListPlan = () => {
         setListPlan([]);
       });
   }, []);
+
   return (
     <LayoutAdmin>
       <AdminTitle>Plan Đã Lưu</AdminTitle>
@@ -25,24 +26,54 @@ const ListPlan = () => {
             href="#"
             className="block jc-between p-6 m-5 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
           >
-            <div>
-              <h3 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Tên Khách Sạn: {plan.hotel_name}
-              </h3>
-              <h3 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Tên Nhà Hàng: {plan.restaurant_name}
-              </h3>
-              <h3 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Địa Điểm: {plan.location_name}
-              </h3>
+            <div className="flex justify-between">
+              <div>
+                <div className="flex">
+                  <p className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Địa Điểm:
+                  </p>
+                  <p className="mb-2 ml-4 text-xl tracking-tight text-gray-900 dark:text-white">
+                    {plan.location[0].location_name} -{" "}
+                    {plan.location[0].location_address}
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Tên Khách Sạn:
+                  </p>
+                  <p className="mb-2 ml-4 text-xl tracking-tight text-gray-900 dark:text-white">
+                    {plan.hotel[0].hotel_name} - {plan.hotel[0].hotel_address}
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Tên Nhà Hàng:
+                  </p>
+                  <p className="mb-2 ml-4 text-xl tracking-tight text-gray-900 dark:text-white">
+                    {plan.restaurant[0].restaurant_name}
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Giờ mở cửa:
+                  </p>
+                  <p className="mb-2 ml-4 text-xl tracking-tight text-gray-900 dark:text-white">
+                    {plan.restaurant[0].restaurant_open_time}
+                  </p>
+                </div>
+                
+              </div>
+              <div className="overflow-hidden">
+                <img
+                  className="inset-0 h-60 w-full object-cover"
+                  src={plan.location[0].image_url}
+                  alt=""
+                />
+              </div>
             </div>
-            <div>
-              <Link to={"search-plan-restaurant"}>
-                <Button variant="contained" color="error">
-                  Xóa plan
-                </Button>
-              </Link>
-            </div>
+            <div className="text-center">
+                  <DeletePlan id={plan.plan_id} />
+                </div>
           </a>
         );
       })}
--- src/services/PlanServices.js
+++ src/services/PlanServices.js
@@ -8,6 +8,14 @@ export const showPlan = async () => {
   return await apiService.get("/plan/show");
 };
 
+export const getPlanByUserName = async (user_name) => {
+  return await apiService.get(`/plan/get_plans_by_user_name?user_name=${user_name}`);
+};
+
+export const deletePlan = async (id) => {
+  return await apiService.delete(`/plan/delete?id=${id}`);
+};
+
 export const getRecommendTransport = async (location_name) => {
   return await apiService.get(
     `/plan/recommend_transport_by_location_name?location_name=${location_name}`
